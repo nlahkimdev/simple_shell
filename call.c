eda8792c9a4eaa4230aa9df5be5a1e2191d4636f
EnGentech <engen.inyang@gmail.com>
2023-02-21 23:00:06 -0800

    edited

--- call.c
+++ call.c
@@ -13,7 +13,7 @@ void prompt(void)
 		int status, lenbuf;
 		size_t bufsize = 0;
 
-		place("$ ");
+		place("#cisfun$ ");
 		lenbuf = getline(&text, &bufsize, stdin);
 		if (lenbuf == -1)
 			exit(98);
